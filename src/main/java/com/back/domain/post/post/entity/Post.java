fce2f0141553091e959007e830e93644594c996f
zunobk <junho4560@naver.com>
2025-12-03 10:18:55 +0900

    PostComment 엔티티 추가, 샘플 데이터도 생성

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,9 +1,18 @@
 package com.back.domain.post.post.entity;
 
+import com.back.domain.post.postComment.entity.PostComment;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REMOVE;
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
@@ -11,6 +20,9 @@ import lombok.NoArgsConstructor;
 public class Post extends BaseEntity {
     private String title;
     private String content;
+
+    @OneToMany(mappedBy = "post", fetch = LAZY, cascade = {PERSIST, REMOVE})
+    private List<PostComment> comments = new ArrayList<>();
 
     public Post(String title, String content) {
         this.title = title;
@@ -20,5 +32,12 @@ public class Post extends BaseEntity {
     public void modify(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public PostComment addComment(String content) {
+        PostComment postComment = new PostComment(this, content);
+        comments.add(postComment);
+
+        return postComment;
     }
 }
